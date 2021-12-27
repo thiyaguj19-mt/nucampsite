ef09c42be6c49f750bec1e8b624e731d04bce5bb
Thiyagarajan Mohan <thiyaguj19@live.com>
2021-12-26 21:04:02 -0800

    week assignment four

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,12 @@ $(function () {
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
+
+    $("#loginButton").click(function() {
+        $('#loginModal').modal('show')
+    });
+
+    $("#reserveButton").click(function() {
+        $('#reserveModal').modal('show')
+    });
 })
